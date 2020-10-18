95af923ad76953d5d68c5e55319f79ff9073f48b
Alfie Thomasson <alfiethomasson0@gmail.com>
2020-10-18 19:12:14 +0100

    Raycast and line render working from Gun.  Can see where gun is aiming when you fire.

---
+++ LagShooter/Assets/Scripts/RayViewer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayViewer : MonoBehaviour
+{
+    public float range = 50.0f;
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponentInParent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lineOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+        Debug.DrawRay(lineOrigin, cam.transform.forward * range, Color.green);
+    }
+}
---
+++ LagShooter/Assets/Scripts/Weapon.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int damage = 1;
+    public float firerate = 0.5f;
+    public float range = 50.0f;
+    public float shotDuration = 0.1f;
+
+    public Transform muzzle;
+
+    private Camera fpsCam;
+    private LineRenderer laser;
+    private float nextShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laser = GetComponent<LineRenderer>();
+        fpsCam = GetComponentInParent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /*  RaycastHit hit;
+            laser.SetPosition(0, muzzle.position);
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3(0.5f, 0.5f, 0.0f));
+            if (Physics.Raycast(rayOrigin,fpsCam.transform.forward, out hit, range))
+            {
+                laser.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laser.SetPosition(1, fpsCam.transform.forward * range);
+            }*/
+        if (Input.GetButtonDown ("Fire1") && Time.time > nextShot) 
+        {
+            nextShot = Time.time + firerate;
+            StartCoroutine(Shot());
+            RaycastHit hit;
+            laser.SetPosition(0, muzzle.position);
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3(0.5f, 0.5f, 0.0f));
+            if (Physics.Raycast(rayOrigin,fpsCam.transform.forward, out hit, range))
+            {
+                laser.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laser.SetPosition(1, fpsCam.transform.forward * range);
+            }
+        }
+    }
+
+    private IEnumerator Shot()
+    {
+        laser.enabled = true;
+        yield return shotDuration;
+        laser.enabled = false;
+    }
+}
